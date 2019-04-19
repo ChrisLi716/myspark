d4a571c0f20a4c731ac97299b157c0c568c96e64
Chris Li <lunli@luns-MacBook-Pro.local>
2019-04-19 11:55:09 +0800

    myspark

---
+++ src/main/java/spark/wordcount.java
@@ -0,0 +1,23 @@
+package spark;
+
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
+public class wordcount {
+	
+	public static void main(String[] args) {
+
+        SparkConf conf = new SparkConf().setMaster("local[1]").setAppName("wordCount");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+        JavaRDD<String> lines = jsc.textFile(args[0]); 
+        JavaRDD<String> words = lines.flatMap(x ->    Arrays.asList(x.split(" ")).iterator()); 
+        JavaPairRDD<String, Integer> counts = words.mapToPair(w -> new Tuple2<String, Integer>(w, 1)).reduceByKey((x, y) -> x + y);  
+        counts.saveAsTextFile(args[1]); jsc.stop();
+    }
+}
